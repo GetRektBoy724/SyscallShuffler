e064eb205f79fc8456d495133c2d25463090f6dd
GєṭRєҡṭBȏʏ724 <virusleader324@gmail.com>
2022-03-28 23:28:52 +0700

    Add SyscallShuffler v0.2
    Changelog :
    - Tidier and more efficient code
    - better looks ig lmao

--- SyscallShuffler.cs
+++ SyscallShuffler.cs
@@ -24,8 +24,6 @@ public class SyscallScrambler {
     public struct SyscallTableEntry {
         public string Name;
         public Int64 OriginalExportAddress;
-        public Int64 ScrambledExportAddress;
-        public Int32 ScrambledExportRVA;
         public IntPtr EATAddress;
         public byte[] Stub;
     }
@@ -45,21 +43,6 @@ public class SyscallScrambler {
         }
     }
 
-    public static unsafe void Copy(byte[] source, int startIndex, IntPtr destination, int length) { // copy from managed to unmanaged
-        if (source == null || source.Length == 0 || destination == IntPtr.Zero || length == 0) {
-            throw new ArgumentNullException("Exception : One or more of the arguments are zero/null!");
-        }
-        if ((startIndex + length) > source.Length) {
-            throw new ArgumentOutOfRangeException("Exception : startIndex and length exceeds the size of source bytes!");
-        }
-        int targetIndex = 0;
-        byte* TargetByte = (byte*)(destination.ToPointer());
-        for (int sourceIndex = startIndex; sourceIndex < (startIndex + length); sourceIndex++) {
-            *(TargetByte + targetIndex) = source[sourceIndex];
-            targetIndex++;
-        }
-    }
-
     public static void Main(string[] args) {
         if (args.Length != 2) {
             Console.WriteLine("Usage : {0} <source ntdll path> <new ntdll path>", Environment.GetCommandLineArgs()[0]);
@@ -67,6 +50,8 @@ public class SyscallScrambler {
             return;
         }
 
+        Console.WriteLine("SyscallShuffler v0.2 - By GetRektBoy724");
+        Console.WriteLine("[*] Preparing and mapping the NTDLL...");
         List<SyscallTableEntry> FunctionList = new List<SyscallTableEntry>();
         List<Int64> AddressHolder = new List<Int64>();
 
@@ -88,54 +73,50 @@ public class SyscallScrambler {
             Marshal.Copy(ModuleRawByte, CurrentSectionOffset, (IntPtr)(ModuleBase.ToInt64() + CurrentSectionRVA), CurrentSectionSize);
         }
 
-        try {
-            // Traverse the PE header in memory
-            Int32 PeHeader = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + 0x3C));
-            Int64 OptHeader = ModuleBase.ToInt64() + PeHeader + 0x18;
-            Int16 Magic = Marshal.ReadInt16((IntPtr)OptHeader);
-            Int64 pExport = 0;
-            if (Magic == 0x010b) {
-                pExport = OptHeader + 0x60;
-            }
-            else {
-                pExport = OptHeader + 0x70;
-            }
-
-            // Read -> IMAGE_EXPORT_DIRECTORY
-            Int32 ExportRVA = Marshal.ReadInt32((IntPtr)pExport);
-            Int32 OrdinalBase = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x10));
-            Int32 NumberOfNames = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x18));
-            Int32 FunctionsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x1C));
-            Int32 NamesRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x20));
-            Int32 OrdinalsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x24));
-
-            for (int i = 0; i < NumberOfNames; i++) {
-                string CurrentFunctionName = Marshal.PtrToStringAnsi((IntPtr)(ModuleBase.ToInt64() + Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + NamesRVA + i * 4))));
-                Int32 CurrentFunctionOrdinal = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + OrdinalsRVA + i * 2)) + OrdinalBase;
-                Int32 CurrentFunctionRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (CurrentFunctionOrdinal - OrdinalBase))));
-                IntPtr CurrentFunctionPtr = (IntPtr)((Int64)ModuleBase + CurrentFunctionRVA);
-
-                // collect all NT* functions and their addresses
-                if (CurrentFunctionName.StartsWith("Nt") && !CurrentFunctionName.StartsWith("Ntdll") && CurrentFunctionName != "NtGetTickCount") {
-                    SyscallTableEntry currententrytable = new SyscallTableEntry();
-                    currententrytable.Name = CurrentFunctionName;
-                    currententrytable.OriginalExportAddress = (Int64)CurrentFunctionPtr;
-                    currententrytable.Stub = new byte[32];
-                    Copy(CurrentFunctionPtr, ref currententrytable.Stub, 0, 32);
-                    currententrytable.EATAddress = (IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (CurrentFunctionOrdinal - OrdinalBase)));
-                    FunctionList.Add(currententrytable);
-                    AddressHolder.Add((long)CurrentFunctionPtr);
-                }
-            }
-        }catch(Exception e) {
-            Console.WriteLine(e);
-            Environment.Exit(0);
+        // Traverse the PE header in memory
+        Int32 PeHeader = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + 0x3C));
+        Int64 OptHeader = ModuleBase.ToInt64() + PeHeader + 0x18;
+        Int16 Magic = Marshal.ReadInt16((IntPtr)OptHeader);
+        Int64 pExport = 0;
+        if (Magic == 0x010b) {
+            pExport = OptHeader + 0x60;
+        }
+        else {
+            pExport = OptHeader + 0x70;
         }
 
-        Console.WriteLine("Randomizing the address holder...");
+        // Read -> IMAGE_EXPORT_DIRECTORY
+        Int32 ExportRVA = Marshal.ReadInt32((IntPtr)pExport);
+        Int32 OrdinalBase = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x10));
+        Int32 NumberOfNames = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x18));
+        Int32 FunctionsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x1C));
+        Int32 NamesRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x20));
+        Int32 OrdinalsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x24));
+
+        for (int i = 0; i < NumberOfNames; i++) {
+            string CurrentFunctionName = Marshal.PtrToStringAnsi((IntPtr)(ModuleBase.ToInt64() + Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + NamesRVA + i * 4))));
+            Int32 CurrentFunctionOrdinal = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + OrdinalsRVA + i * 2)) + OrdinalBase;
+            Int32 CurrentFunctionRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (CurrentFunctionOrdinal - OrdinalBase))));
+            IntPtr CurrentFunctionPtr = (IntPtr)((Int64)ModuleBase + CurrentFunctionRVA);
+
+            // collect all NT* functions and their addresses
+            if (CurrentFunctionName.StartsWith("Nt") && !CurrentFunctionName.StartsWith("Ntdll") && CurrentFunctionName != "NtGetTickCount") {
+                SyscallTableEntry currententrytable = new SyscallTableEntry();
+                currententrytable.Name = CurrentFunctionName;
+                currententrytable.OriginalExportAddress = (Int64)CurrentFunctionPtr;
+                currententrytable.Stub = new byte[32];
+                Copy(CurrentFunctionPtr, ref currententrytable.Stub, 0, 32);
+                currententrytable.EATAddress = (IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (CurrentFunctionOrdinal - OrdinalBase)));
+                FunctionList.Add(currententrytable);
+                AddressHolder.Add((long)CurrentFunctionPtr);
+            }
+        }
+
+        Console.WriteLine("[*] Shuffling the addresses...");
         // scramble the position
         Random rng = new Random();  
         AddressHolder.ScrambleTheList(rng);
+
         // check if there is any scrambled address that are the same with the original address
         while (true) {
             bool ClearToContinue = true;
@@ -150,24 +131,14 @@ public class SyscallScrambler {
                 break;
         }
 
-        // get the new address
+        Console.WriteLine("[*] Placing the syscall stubs to the new addresses...");
+        // copy the function stubs to the randomized addresses
         for (int i = 0; i < FunctionList.Count; i++) {
-            Console.WriteLine("Getting the randomized address for {0}...", FunctionList[i].Name);
-            SyscallTableEntry currententrytable = FunctionList[i];
-            currententrytable.ScrambledExportAddress = AddressHolder[i];
-            currententrytable.ScrambledExportRVA = (Int32)(currententrytable.ScrambledExportAddress - (long)ModuleBase);
-            FunctionList[i] = currententrytable;
+            Marshal.Copy(FunctionList[i].Stub, 0, (IntPtr)AddressHolder[i], 32);
+            Marshal.WriteInt32(FunctionList[i].EATAddress, (Int32)(AddressHolder[i] - (long)ModuleBase));
         }
 
-        // update the stub and the EAT
-        for (int i = 0; i < FunctionList.Count; i++) {
-            Console.WriteLine("Applying patch on {0} for {1}...", FunctionList[i].ScrambledExportAddress.ToString("X4"), FunctionList[i].Name);
-            IntPtr ScrambledExportAddress = (IntPtr)FunctionList[i].ScrambledExportAddress;
-            IntPtr EATAddress = (IntPtr)FunctionList[i].EATAddress;
-            Marshal.Copy(FunctionList[i].Stub, 0, (IntPtr)FunctionList[i].ScrambledExportAddress, 32);
-            Marshal.WriteInt32(FunctionList[i].EATAddress, FunctionList[i].ScrambledExportRVA);
-        }
-
+        Console.WriteLine("[*] Unmapping and finishing...");
         // unmap it
         byte[] NewNTDLL = new byte[ModuleRawByte.Length];
         Marshal.Copy(ModuleBase, NewNTDLL, 0, SizeOfHeaders);
@@ -186,5 +157,6 @@ public class SyscallScrambler {
         }
 
         File.WriteAllBytes(args[1], NewNTDLL);
+        Console.WriteLine("[+] Done!");
     }
 }
